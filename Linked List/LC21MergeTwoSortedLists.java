c099e5bfd839b1716d3f0bc0107da62c94b8b9bd
DavidHsin <daxian2392@gmail.com>
2019-05-22 16:20:39 +0800

    Create LC21MergeTwoSortedLists.java

---
+++ Linked List/LC21MergeTwoSortedLists.java
@@ -0,0 +1,49 @@
+/**
+Merge two sorted linked lists and return it as a new list. The new list should be made by splicing together the nodes of the first two lists.
+
+Example:
+
+Input: 1->2->4, 1->3->4
+Output: 1->1->2->3->4->4
+ */
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+        //it's related to operations in head node, so dummyNode is a better choice
+        ListNode dummyNode = new ListNode(0);
+        ListNode cur = dummyNode;
+        
+        //once one linked list is empty, while loop is over
+        while(l1 != null && l2 != null) {
+            if(l1.val < l2.val) {
+                cur.next = l1;
+                l1 = l1.next;
+            }else {
+                cur.next = l2;
+                l2 = l2.next;
+            }
+            //After adding node, cur = cur.next;
+            cur = cur.next;
+        }
+        
+        //case1: if l1 is larger than l2
+        if(l1 != null) {
+            cur.next = l1;
+        }
+        
+        //case2: if l2 is larger than l1
+        if(l2 != null) {
+            cur.next = l2;
+        }
+        
+        return dummyNode.next;
+    }
+}
