8e29ab9e74222605345ac5d9db94600b0ff04c89
DavidHsin <daxian2392@gmail.com>
2019-05-31 23:05:56 +0800

    Create FirstTargetInASortedArray.java

---
+++ Binary Search/FirstTargetInASortedArray.java
@@ -0,0 +1,55 @@
+package leetcode;
+
+/**
+ * Given a sorted array, return the first occurrence of the target
+ * 
+ * Example:
+ * arr = [4, 5, 5, 5, 5, 5, 5];
+ * target = 5;
+ * return 1;
+ * 
+ * arr = [1, 2, 3, 4, 5, 6, 6, 7];
+ * target = 6;
+ * return 5;
+ * 
+ * */
+
+public class FirstTargetInASortedArray {
+	
+	public static int firstTarget(int arr[], int target) {
+		if(arr == null || arr.length == 0) {
+			return -1;
+		}
+		
+		if(arr.length == 1) {
+			return arr[0];
+		}
+		
+		int left = 0;
+		int right = arr.length - 1;
+		while(left + 1 < right) {	//it ensures the program ends when there are two elements in this array
+			int mid = left + (right - left) / 2;
+			if(arr[mid] == target) {
+				right = mid;		//it keeps at least one target in remaining array
+			}else if(arr[mid] < target) {
+				left = mid + 1;
+			}else {
+				right = mid - 1;
+			}
+		}
+		
+		if(arr[left] == target) {
+			return left;
+		}else if(arr[right] == target) {
+			return right;
+		}
+		return -1;
+	}
+
+	public static void main(String[] args) {
+		int[] arr = {1, 2, 3, 4, 5, 6, 6, 7};
+		int target = 6;
+		System.out.println(firstTarget(arr, target));
+	}
+
+}
