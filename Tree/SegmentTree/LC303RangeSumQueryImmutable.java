195ce46e552412b47bcadcb3d8c95a3248270d49
DavidHsin <daxian2392@gmail.com>
2019-06-02 13:14:43 +0800

    Create LC303RangeSumQueryImmutable.java

---
+++ Tree/SegmentTree/LC303RangeSumQueryImmutable.java
@@ -0,0 +1,158 @@
+/**
+
+Given an integer array nums, find the sum of the elements between indices i and j (i ≤ j), inclusive.
+
+Example:
+Given nums = [-2, 0, 3, -5, 2, -1]
+
+sumRange(0, 2) -> 1
+sumRange(2, 5) -> -1
+sumRange(0, 5) -> -3
+ */
+
+class NumArray {
+    
+    public interface Merger<E> {
+        
+	    E merge(E a, E b);
+    }
+        
+    public class SegmentTree<E> {
+
+        private E[] tree;
+        private E[] data;
+        private Merger<E> merger;
+
+        public SegmentTree(E[] arr, Merger<E> merger){
+
+            this.merger = merger;
+
+            data = (E[])new Object[arr.length];
+            for(int i = 0 ; i < arr.length ; i ++)
+                data[i] = arr[i];
+
+            tree = (E[])new Object[4 * arr.length];
+            buildSegmentTree(0, 0, arr.length - 1);
+        }
+
+        // 在treeIndex的位置创建表示区间[l...r]的线段树
+        private void buildSegmentTree(int treeIndex, int l, int r){
+
+            if(l == r){
+                tree[treeIndex] = data[l];
+                return;
+            }
+
+            int leftTreeIndex = leftChild(treeIndex);
+            int rightTreeIndex = rightChild(treeIndex);
+
+            int mid = l + (r - l) / 2;
+            buildSegmentTree(leftTreeIndex, l, mid);
+            buildSegmentTree(rightTreeIndex, mid + 1, r);
+
+            tree[treeIndex] = merger.merge(tree[leftTreeIndex], tree[rightTreeIndex]);
+        }
+
+        public int getSize(){
+            return data.length;
+        }
+
+        public E get(int index){
+            if(index < 0 || index >= data.length)
+                throw new IllegalArgumentException("Index is illegal.");
+            return data[index];
+        }
+
+        // 返回完全二叉树的数组表示中，一个索引所表示的元素的左孩子节点的索引
+        private int leftChild(int index){
+            return 2*index + 1;
+        }
+
+        // 返回完全二叉树的数组表示中，一个索引所表示的元素的右孩子节点的索引
+        private int rightChild(int index){
+            return 2*index + 2;
+        }
+
+        //返回区间[queryL, queryR]前闭后闭的值
+        public E query(int queryL, int queryR) {
+
+            //边界检查
+            if(queryL < 0 || queryL >= data.length || queryR < 0 || queryR >= data.length || queryL > queryR) {
+                throw new IllegalArgumentException("Index is illegal.");
+            }
+
+            return query(0, 0, data.length - 1, queryL, queryR);
+        }
+
+        //在以treeIndex为根的线段树中[l...r]的范围里，搜索区间[queryL...queryR]的值
+        //这里l = 0, r = data.length - 1
+        private E query(int treeIndex, int l, int r, int queryL, int queryR) {
+
+            //base case
+            if(l == queryL && r == queryR) {
+                return tree[treeIndex];
+            }
+
+            //recursive rules
+            //分为三种，向左找，向右找，左右都有
+            int mid = l + (r - l) / 2;
+            int leftTreeIndex = leftChild(treeIndex);
+            int rightTreeIndex = rightChild(treeIndex);
+
+            if(queryL >= mid + 1) {		//一头扎向右孩子
+                return query(rightTreeIndex, mid + 1, r, queryL, queryR);
+            }else if(queryR <= mid) {	//一头扎向左孩子
+                return query(leftTreeIndex, l, mid, queryL, queryR);
+            }
+
+            //左孩子和有孩子都有的时候
+            E leftResult = query(leftTreeIndex, l, mid, queryL, mid);
+            E rightResult = query(rightTreeIndex, mid + 1, r, mid + 1, queryR);
+            return merger.merge(leftResult, rightResult);
+        }
+
+        @Override
+        public String toString(){
+            StringBuilder res = new StringBuilder();
+            res.append('[');
+            for(int i = 0 ; i < tree.length ; i ++){
+                if(tree[i] != null)
+                    res.append(tree[i]);
+                else
+                    res.append("null");
+
+                if(i != tree.length - 1)
+                    res.append(", ");
+            }
+            res.append(']');
+            return res.toString();
+        }
+    }
+    
+    private SegmentTree<Integer> segmentTree;
+
+    public NumArray(int[] nums) {
+        
+        if(nums.length > 0) {
+            Integer[] data = new Integer[nums.length];
+            for(int i = 0; i < nums.length; i++) {
+                data[i] = nums[i];
+            }
+            segmentTree = new SegmentTree<>(data, (a, b) -> a + b);
+        }
+    }
+    
+    public int sumRange(int i, int j) {
+        
+        if(segmentTree == null) {
+            throw new IllegalArgumentException("Segment Tree is null.");
+        }
+        return segmentTree.query(i, j);
+    }
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * int param_1 = obj.sumRange(i,j);
+ */
