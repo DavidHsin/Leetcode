bd7d9d3adee15a700179c0857cee00fd7889d7b6
DavidHsin <daxian2392@gmail.com>
2019-06-06 17:30:26 +0800

    Create LC211AddAndSearchWord.java

---
+++ Tree/Trie/LC211AddAndSearchWord.java
@@ -0,0 +1,96 @@
+import java.util.TreeMap;
+
+/**
+Design a data structure that supports the following two operations:
+
+void addWord(word)
+bool search(word)
+search(word) can search a literal word or a regular expression string containing only letters a-z or .. A . means it can represent any one letter.
+
+Example:
+
+addWord("bad")
+addWord("dad")
+addWord("mad")
+search("pad") -> false
+search("bad") -> true
+search(".ad") -> true
+search("b..") -> true
+ */
+
+class WordDictionary {
+	
+	private class Node {
+		
+		public boolean isWord;
+		public TreeMap<Character, Node> next;
+		
+		public Node(boolean isWord) {
+			this.isWord = isWord;
+			next = new TreeMap<>();
+		}
+		
+		public Node() {
+			this(false);
+		}
+	}
+	
+	private Node root;
+
+    /** Initialize your data structure here. */
+    public WordDictionary() {
+        root = new Node();
+    }
+    
+    /** Adds a word into the data structure. */
+    public void addWord(String word) {
+    	Node cur = root;
+		for(int i = 0; i < word.length(); i++) {
+			char c = word.charAt(i);
+			if(cur.next.get(c) == null) {
+				cur.next.put(c, new Node());
+			}
+			cur = cur.next.get(c);
+		}
+		if(!cur.isWord) {
+			cur.isWord = true;
+		}
+    }
+    
+    /** Returns if the word is in the data structure. A word could contain the dot character '.' to represent any one letter. */
+    public boolean search(String word) {
+        
+    	return match(root, word, 0);
+    }
+    
+    private boolean match(Node node, String word, int index) {
+    	
+    	//base case
+    	if(index == word.length()) {
+    		return node.isWord;
+    	}
+    	
+    	//recursive rules
+    	char c = word.charAt(index);
+    	if(c != '.') {
+    		if(node.next.get(c) == null) {
+    			return false;
+    		}
+    		return match(node.next.get(c), word, index + 1);
+    	}else {
+    		for(char nextChar: node.next.keySet()) {
+    			if(match(node.next.get(nextChar), word, index + 1)) {
+    				return true;
+    			}
+    		}
+    		return false;
+    	}
+    }
+}
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary obj = new WordDictionary();
+ * obj.addWord(word);
+ * boolean param_2 = obj.search(word);
+ */
