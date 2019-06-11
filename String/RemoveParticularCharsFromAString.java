b6d065f470007c15cfa0236ae9f4e45c00003e75
DavidHsin <daxian2392@gmail.com>
2019-06-11 10:44:51 +0800

    Create RemoveParticularCharsFromAString.java

---
+++ String/RemoveParticularCharsFromAString.java
@@ -0,0 +1,24 @@
+package leetcode;
+
+public class RemoveParticularCharsFromAString {
+	
+	public static String charRemoval(String input) {
+		//it's crucial to use "StringBuilder" to solve this problem, that's easier.
+		StringBuilder s = new StringBuilder(input);
+		int i = 0;
+		for(int j = 0; j < input.length(); j++) {
+			if(s.charAt(j) != 'u' && s.charAt(j) != 'n') {
+				s.setCharAt(i, s.charAt(j));
+				i++;
+			}
+		}
+		return s.substring(0, i);
+		//pay attention to the usage of "setCharAt", "charAt", "substring"
+	}
+	
+	
+	public static void main(String[] args) {
+		String input = "studennt";
+		System.out.println(charRemoval(input));
+	}
+}
