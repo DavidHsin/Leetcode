8b21fc3b2a2a161ff5ac81028069bdd10b36ce52
DavidHsin <daxian2392@gmail.com>
2019-06-17 17:22:49 +0800

    Update LongestIncreasingSubarray.java

--- Dynamic Programming/LongestIncreasingSubarray.java
+++ Dynamic Programming/LongestIncreasingSubarray.java
@@ -28,6 +28,40 @@ package leetcode;
 public class LongestIncreasingSubarray {
 	
 	public static int lenOfLonIncSubarray(int[] arr) {
+		//return demo1(arr);
+		
+		int lengthOfArray = arr.length;
+		/**
+		 * index =  0  1  2  3  4  5  6  7
+		 * array = [7, 2, 3, 1, 5, 8, 9, 6]
+		 *  M[]  =  1  1  2  1  2  3  4  1
+		 *  
+		 *  Method 1 of wasting extra space to store every max length
+		 */
+		int[] MaxLength = new int[lengthOfArray];
+		
+		MaxLength[0] = 1;
+		for(int i = 1; i < lengthOfArray; i++) {
+			if(arr[i] > arr[i - 1]) {
+				MaxLength[i] = MaxLength[i - 1] + 1;
+			}else {
+				MaxLength[i] = 1;
+			}
+		}
+		
+		int global_max = 0;
+		for(int i = 0; i < MaxLength.length; i++) {
+			if(MaxLength[i] > global_max) {
+				global_max = MaxLength[i];
+			}
+		}
+		return global_max;
+	}
+
+	/**
+	 * Method 2 of saving space
+	 */
+	private static int demo1(int[] arr) {
 		int n = arr.length;
 		//Base case: only one element, len[0] = 1, global_max[0] = 1
 		int len = 1;
