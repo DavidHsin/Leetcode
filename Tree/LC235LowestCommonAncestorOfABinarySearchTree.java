2e82249ced7aa59e8415cad8e187627a981000a9
DavidHsin <daxian2392@gmail.com>
2019-06-26 18:43:21 +0800

    Update LC235LowestCommonAncestorOfABinarySearchTree.java

--- Tree/LC235LowestCommonAncestorOfABinarySearchTree.java
+++ Tree/LC235LowestCommonAncestorOfABinarySearchTree.java
@@ -21,18 +21,27 @@ Explanation: The LCA of nodes 2 and 8 is 6.
  *     TreeNode(int x) { val = x; }
  * }
  */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root == null || root == p || root == q) {
+        if(root == null) {
             return root;
         }
         
-        TreeNode left = lowestCommonAncestor(root.left, p, q);
-        TreeNode right = lowestCommonAncestor(root.right, p, q);
-        
-        if(left != null && right != null) {
+        if(p.val < root.val && q.val < root.val) {
+            return lowestCommonAncestor(root.left, p, q);
+        }else if(p.val > root.val && q.val > root.val) {
+            return lowestCommonAncestor(root.right, p, q);
+        }else {
             return root;
         }
-        return left == null ? right : left;
     }
 }
