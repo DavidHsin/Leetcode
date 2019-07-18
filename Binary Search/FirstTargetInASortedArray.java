1e5bdec88a179e1e47740f32237da507a4faf7eb
DavidHsin <daxian2392@gmail.com>
2019-07-18 11:36:24 -0700

    Update FirstTargetInASortedArray.java

--- Binary Search/FirstTargetInASortedArray.java
+++ Binary Search/FirstTargetInASortedArray.java
@@ -17,21 +17,23 @@ package leetcode;
 public class FirstTargetInASortedArray {
 	
 	public static int firstTarget(int arr[], int target) {
+		
+		//corner case 1
 		if(arr == null || arr.length == 0) {
 			return -1;
 		}
 		
-		if(arr.length == 1) {
-			return arr[0];
+		//corner case 2
+		if(arr.length == 1 && arr[0] == target) {
+			return 0;
 		}
 		
 		int left = 0;
 		int right = arr.length - 1;
-		while(left + 1 < right) {	//it ensures the program ends when there are two elements in this array
-			//unsigned right shift
+		while(left + 1 < right) {
 			int mid = (left + right) >>> 1;
 			if(arr[mid] == target) {
-				right = mid;		//it keeps at least one target in remaining array
+				right = mid;	//the different point
 			}else if(arr[mid] < target) {
 				left = mid + 1;
 			}else {
@@ -39,18 +41,24 @@ public class FirstTargetInASortedArray {
 			}
 		}
 		
+		//compare the remaining two elements
 		if(arr[left] == target) {
 			return left;
 		}else if(arr[right] == target) {
 			return right;
+		}else {
+			return -1;
 		}
-		return -1;
 	}
 
 	public static void main(String[] args) {
-		int[] arr = {1, 2, 3, 4, 5, 6, 6, 7};
-		int target = 6;
+		int[] arr = {0, 1, 2, 3, 4, 6};
+		int target = 5;
 		System.out.println(firstTarget(arr, target));
 	}
-
 }
+
+/**
+ * Time complexity: O(logn)
+ * Space complexity: O(logn)
+ */
