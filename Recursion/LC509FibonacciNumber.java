146a1bee536f1977753d67ed3143c3c054bd09c5
DavidHsin <daxian2392@gmail.com>
2019-07-18 10:04:26 -0700

    Update LC509FibonacciNumber.java

--- Recursion/LC509FibonacciNumber.java
+++ Recursion/LC509FibonacciNumber.java
@@ -24,19 +24,59 @@ Output: 3
 Explanation: F(4) = F(3) + F(2) = 2 + 1 = 3.
  */
 
-class Solution {
+class Solution1 {
     public int fib(int N) {
-        //Base case: smallest problem to slove
-        if (N == 0 || N == 1) {
+        //base case
+        if(N == 0 || N == 1) {
             return N;
         }
         
-        //Recursive rules: how to make the problem smaller
+        //recursive rules
         return fib(N - 1) + fib(N - 2);
     }
 }
 
-/*
- * Time complexity: O(1 + 2 + 4 + 8 + ... + 2^n) = O(2^n)
- * Space complexity: O(1)
+/**
+ * for example, if N == 4, according to the recursive rules,
+ * we painted this picture.
+ *              F(4) --------------- 1
+ *            /      \
+ *           F(3)     F(2) --------- 2
+ *          /   \     /    \
+ *         F(2) F(1) F(1) F(0) ----- 4
+ *        / \
+ *      F(1) F(0) ------------------ 8
+ * 
+ * Time complexity: 1 + 2 + 4 + 8 + ... = 2 ^ 0 + 2 ^ 1 + 2 ^ 2 + ... + 2 ^ (n-1) = O(2^n)
+ * Space complexity: According to Von Neumann architecture, program and data are first stored
+ *                   in the memory, when computer executes the program, instructions are 
+ *                   automatically and sequentially fetched from memory one by one. The concept
+ *                   is called a sequential execution program.
+ *                   So in this fibonacci program, instructions would follow like DFS: 
+ *                   F(4) -> F(3) -> F(2) -> F(1): O(n)
+ * 
+ */
+
+class Solution2 {
+    public int fib(int N) {
+        
+        int[] fiboFound = new int[N + 2];	//如果此处改成N+1会出现"java.lang.ArrayIndexOutOfBoundsException: 1"
+        fiboFound[0] = 0;
+        fiboFound[1] = 1;
+        for(int i = 2; i <= N; i++) {
+            fiboFound[i] = fiboFound[i - 1] + fiboFound[i - 2];
+        }
+        return fiboFound[N];
+    }
+}
+
+/**
+ * 
+ * Dynamic programming：把表格里的value填满
+ * index  0 1 2 3 4 5 6 ... 1000
+ * F(n)   0 1 1 2 3 5 8 ... F(1000)
+ * 
+ * Time complexity: O(n)
+ * 
+ * */
  */
