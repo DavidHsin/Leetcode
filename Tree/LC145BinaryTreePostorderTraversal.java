5ed17e484cbe5f0bd04652f0631f8cbb3398277b
DavidHsin <daxian2392@gmail.com>
2019-07-31 12:06:00 -0700

    Create LC145BinaryTreePostorderTraversal.java

---
+++ Tree/LC145BinaryTreePostorderTraversal.java
@@ -0,0 +1,49 @@
+/**
+Given a binary tree, return the postorder traversal of its nodes' values.
+
+Example:
+
+Input: [1,null,2,3]
+   1
+    \
+     2
+    /
+   3
+
+Output: [3,2,1]
+ */
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        helper(root, res);
+        return res;
+    }
+    
+    public void helper(TreeNode root, List<Integer> res) {
+        //base case
+        if(root == null) {
+            return;
+        }
+        
+        //recursive rules
+        if(root.left != null) {
+            helper(root.left, res);
+        }
+        
+        if(root.right != null) {
+            helper(root.right, res);
+        }
+        
+        res.add(root.val);
+    }
+}
